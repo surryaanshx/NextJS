4f1d1b9601bd47b4531e731698d6cb7fe4b38ed0
surryaansh <suryanshs1804@gmail.com>
2026-01-24 13:57:43 +0530

    fixed font error

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+        serif: ['var(--font-playfair)', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
